b16c6b94a6fd2339d183179fddde94c2bd68ac88
zhangjing <892295834@qq.com>
2025-06-16 09:44:03 +0800

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.less'
+})
+export class App {
+  protected title = 'webapp';
+}
