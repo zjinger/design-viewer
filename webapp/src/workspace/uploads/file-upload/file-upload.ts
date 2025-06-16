a34ec8142788625f8e1f740ca5fa0229e6c2d533
zhangjing <892295834@qq.com>
2025-06-16 16:27:41 +0800

    feat:添加ng-zorro

--- webapp/src/workspace/uploads/file-upload/file-upload.ts
+++ webapp/src/workspace/uploads/file-upload/file-upload.ts
@@ -1,8 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam, NzUploadModule } from 'ng-zorro-antd/upload';
 @Component({
   selector: 'app-file-upload',
-  imports: [],
+  imports: [CommonModule, NzIconModule, NzUploadModule],
   templateUrl: './file-upload.html',
   styleUrl: './file-upload.less',
 })
-export class FileUpload {}
+export class FileUpload {
+  constructor(private messageService: NzMessageService) {}
+
+  handleChange({ file, fileList }: NzUploadChangeParam): void {
+    const status = file.status;
+    if (status !== 'uploading') {
+      console.log(file, fileList);
+    }
+    if (status === 'done') {
+      this.messageService.success(`${file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      this.messageService.error(`${file.name} file upload failed.`);
+    }
+  }
+}
